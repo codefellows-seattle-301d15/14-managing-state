0c991b80036398784f64f25ea36c84cfb50382ca
Geno Guerrero <geno.guerrero@gmail.com>
2016-12-15 13:46:05 -0800

    finished

--- starter-code/scripts/controllers/articleController.js
+++ starter-code/scripts/controllers/articleController.js
@@ -11,7 +11,7 @@
     }
   };
 
-  // COMMENT: What does this method do?  What is it's execution path?
+  // COMMENT: What does this method do?  What is it's execution path?  Takes in a parameter and loads this article by its id.  Also calls the authorData method using the id. articleController.index sends data to the /article/:id page.
   articleController.loadById = function(ctx, next) {
     var articleData = function(article) {
       ctx.articles = article;
@@ -20,7 +20,7 @@
     Article.findWhere('id', ctx.params.id, articleData);
   };
 
-  // COMMENT: What does this method do?  What is it's execution path?
+  // COMMENT: What does this method do?  What is it's execution path? This loads data from /author/:authorName' to load the selected author and grab the corresponding articles.
   articleController.loadByAuthor = function(ctx, next) {
     var authorData = function(articlesByAuthor) {
       ctx.articles = articlesByAuthor;
@@ -32,7 +32,7 @@
     );
   };
 
-  // COMMENT: What does this method do?  What is it's execution path?
+  // COMMENT: What does this method do?  What is it's execution path? This loads data from /author/:categoryName' to load the selected category and grab the corresponding articles.
   articleController.loadByCategory = function(ctx, next) {
     var categoryData = function(articlesInCategory) {
       ctx.articles = articlesInCategory;
@@ -42,7 +42,7 @@
     Article.findWhere('category', ctx.params.categoryName, categoryData);
   };
 
-  // COMMENT: What does this method do?  What is it's execution path?
+  // COMMENT: What does this method do?  What is it's execution path? This fetches all data to load the articles. Execution is the home page.
   articleController.loadAll = function(ctx, next) {
     var articleData = function(allArticles) {
       ctx.articles = Article.allArticles;
