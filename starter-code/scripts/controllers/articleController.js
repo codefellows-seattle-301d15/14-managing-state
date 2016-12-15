7af25c58e66f8efd1079f4a3fa18d20583433ae4
irvinemd55 <irvinedowning@gmail.com>
2016-12-15 13:53:18 -0800

    comments added

--- starter-code/scripts/controllers/articleController.js
+++ starter-code/scripts/controllers/articleController.js
@@ -4,7 +4,7 @@
   Article.createTable();
 
   articleController.index = function(ctx, next) {
-    if(ctx.articles.length) {
+    if(ctx.articles.length) { // "ctx" (object) created in .loadAll
       articleView.index(ctx.articles);
     } else {
       page('/');
@@ -13,6 +13,10 @@
 
   // COMMENT: What does this method do?  What is it's execution path?
   articleController.loadById = function(ctx, next) {
+    //middleware function with context and next parameter
+    //execution path page -> loadById -> findWhere (uses SQL command
+    //to check 3 parameters to pull JSON file )-> index adds context
+    //to the view -> articleView.index shows the article
     var articleData = function(article) {
       ctx.articles = article;
       next();
@@ -22,9 +26,15 @@
 
   // COMMENT: What does this method do?  What is it's execution path?
   articleController.loadByAuthor = function(ctx, next) {
+    //context and next letting us know it's a middleware function
+      //execution path page -> loadByAuthor ->
+      //.findWhere filters articles based on author name
+      //index adds the context to the view -> articleView.index
+      //shows the article
     var authorData = function(articlesByAuthor) {
       ctx.articles = articlesByAuthor;
       next();
+      //filters articles based on author and then follows up with article.index
     };
 
     Article.findWhere(
@@ -34,7 +44,13 @@
 
   // COMMENT: What does this method do?  What is it's execution path?
   articleController.loadByCategory = function(ctx, next) {
+    //context and next let us know that it's a middleware function
+    //execution path page -> loadByCategory ->
+    //.findWhere filters articles based on category name
+    //index adds the context to the view -> articleView.index
+    //shows the article
     var categoryData = function(articlesInCategory) {
+      //filters articles by category and calls article.index to populate page
       ctx.articles = articlesInCategory;
       next();
     };
@@ -44,6 +60,11 @@
 
   // COMMENT: What does this method do?  What is it's execution path?
   articleController.loadAll = function(ctx, next) {
+    //"ctx" is context; "next" lets you know it's a middleware function
+    //execution path page -> loadAll ->
+    //index adds the context to the view -> articleView.index
+    //shows the article
+    //fetchAll fetches all the articles from the JSON file
     var articleData = function(allArticles) {
       ctx.articles = Article.allArticles;
       next();
--- starter-code/scripts/controllers/routes.js
+++ starter-code/scripts/controllers/routes.js
@@ -1,29 +1,30 @@
 page('/',
-  articleController.loadAll,
+  articleController.loadAll, //loads all articles
   articleController.index);
 
-page('/about', aboutController.index);
+page('/about', aboutController.index); //calls index from about section
 
-page('/admin', adminController.index);
+page('/admin', adminController.index); //calls index from admin section
 
 page('/article/:id',
-  articleController.loadById,
+  articleController.loadById, //middleware filter by id
   articleController.index);
 
 // Redirect home if the default filter option is selected:
-page('/category', '/');
-page('/author', '/');
+page('/category', '/'); //sends user to homepage
+page('/author', '/'); //sends user to homepage
 
 page('/author/:authorName',
-  articleController.loadByAuthor,
+  articleController.loadByAuthor, //middleware filter by author
   articleController.index);
 
 page('/category/:categoryName',
-  articleController.loadByCategory,
+  articleController.loadByCategory, //middleware filter by category
   articleController.index);
 
 page('*', function(){
+  //if the user tries to type in a route that doesn't exist
   $('body').text('Not found!');
 });
 
-page();
+page(); //calls page function
